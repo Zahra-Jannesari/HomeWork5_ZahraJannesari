7567806f99cdaa99cd13477dca8b509a91d10975
Zarisa <jannesari.zarisa@gmail.com>
2022-02-06 20:46:13 +0330

    add required classes and primary properties and functions

--- src/main/kotlin/question 2.kt
+++ src/main/kotlin/question 2.kt
@@ -1,3 +1,48 @@
- fun main(){
+abstract class Employee{
+ abstract var name: String
+ abstract var age: Int
+ abstract val hourRate: Double
+ abstract fun salary(hours: Double): Double
+ override fun toString(): String {
+  return "name: $name age: $age hourRate: $hourRate"
+ }
+}
+class Manager:Employee(){
+ override var name: String
+  get() = TODO("Not yet implemented")
+  set(value) {}
+ override var age: Int
+  get() = TODO("Not yet implemented")
+  set(value) {}
+ override val hourRate: Double
+  get() = TODO("Not yet implemented")
+
+ override fun salary(hours: Double): Double {
+  TODO("Not yet implemented")
+ }
+
+}
+class  Clerk:Employee(){
+ override var name: String
+  get() = TODO("Not yet implemented")
+  set(value) {}
+ override var age: Int
+  get() = TODO("Not yet implemented")
+  set(value) {}
+ override val hourRate: Double
+  get() = TODO("Not yet implemented")
+
+ override fun salary(hours: Double): Double {
+  TODO("Not yet implemented")
+ }
+
+}
+data class Customer(var name:String, var age:Int,var gender:Gender){
+
+}
+enum class Gender{
+ Male,Female
+}
+fun main(){
 
  }
