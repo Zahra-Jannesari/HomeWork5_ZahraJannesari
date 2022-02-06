2f79fad82bf50b2e5292a7549a0aae370249bdd1
Zarisa <jannesari.zarisa@gmail.com>
2022-02-06 15:01:19 +0330

    add interfaces

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,21 @@
+
+interface Vehicle{
+    val name:String
+}
+interface PublicTransportationVehicle:Vehicle {
+    fun maxSpeed()
+    fun maxCapacity()
+    override val name: String
+}
+interface ServiceTransportationVehicle:Vehicle {
+    fun maxSpeed()
+    fun maxCapacity()
+    override val name: String
+}
+interface PrivateTransportationVehicle:Vehicle {
+    fun maxSpeed()
+    fun maxCapacity()
+    override val name: String
+}
 fun main() {
 }
