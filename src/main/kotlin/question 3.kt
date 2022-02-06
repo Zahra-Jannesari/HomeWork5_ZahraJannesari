bf34c0e14ddee79fedc12bb5f77b7b9fb439fa1e
Zarisa <jannesari.zarisa@gmail.com>
2022-02-07 00:32:50 +0330

    add fun compare in q3. the question may not be completed yet.

--- src/main/kotlin/question 3.kt
+++ src/main/kotlin/question 3.kt
@@ -1,7 +1,23 @@
 fun main(){
-
+    var  check=CheckVariables(2L,8F)
+    println(check.compare())
 }
 
-class CheckVariables<T>(var firstVariable:T,var secondVariable:T){
-
+class CheckVariables<T>(private var firstVariable:T,private var secondVariable:T){
+    fun compare():Any?{
+        return if (firstVariable!!::class==secondVariable!!::class){
+            when(firstVariable){
+                is Byte-> maxOf(firstVariable as Byte,secondVariable as Byte)
+                is Short-> maxOf(firstVariable as Short,secondVariable as Short)
+                is Int-> maxOf(firstVariable as Int,secondVariable as Int)
+                is Long-> maxOf(firstVariable as Long,secondVariable as Long)
+                is Float-> maxOf(firstVariable as Float,secondVariable as Float)
+                is Double-> maxOf(firstVariable as Double,secondVariable as Double)
+                else-> null
+            }
+        }else{
+            println("Variables do not have same types.")
+            null
+        }
+    }
 }
