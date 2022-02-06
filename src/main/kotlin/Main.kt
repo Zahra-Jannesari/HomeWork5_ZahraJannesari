9ea2577a4cc99fe8eba0b2ff6316369f8abe00da
Zarisa <jannesari.zarisa@gmail.com>
2022-02-06 15:11:13 +0330

    add enum VehicleName change name Type from String to VehicleName

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,25 @@
-
+enum class VehicleName{
+    Public_bus, Public_subway,
+    Service_ambulance, Service_truck,
+    Private_Benz,Private_van
+}
 interface Vehicle{
-    val name:String
+    val name:VehicleName
 }
 interface PublicTransportationVehicle:Vehicle {
     fun maxSpeed()
     fun maxCapacity()
-    override val name: String
+    override val name:VehicleName
 }
 interface ServiceTransportationVehicle:Vehicle {
     fun maxSpeed()
     fun maxCapacity()
-    override val name: String
+    override val name:VehicleName
 }
 interface PrivateTransportationVehicle:Vehicle {
     fun maxSpeed()
     fun maxCapacity()
-    override val name: String
+    override val name:VehicleName
 }
 fun main() {
 }
