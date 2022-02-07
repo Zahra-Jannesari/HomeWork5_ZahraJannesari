539b9c79231ef2a65d307c0316ea9ae1b9315dec
Zarisa <jannesari.zarisa@gmail.com>
2022-02-07 23:11:47 +0330

    add required prints in method

--- src/main/kotlin/question 5.kt
+++ src/main/kotlin/question 5.kt
@@ -2,6 +2,7 @@ fun <T>List<T>.sliceAtGivenIndex(index:Int){
     if(index in 0 until this.size){
         var firstList=this.slice(0..index)
         var secondList=this.slice(index+1 until this.size)
+        println("$firstList,$secondList")
     }else{
         println("Index is greater than list size.")
     }
