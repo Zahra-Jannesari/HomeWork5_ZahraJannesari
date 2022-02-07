02db1d666a04a3c31bb574842854cf92756eb1d0
Zarisa <jannesari.zarisa@gmail.com>
2022-02-07 21:52:01 +0330

    add an array list and put some arguments in it to test the method. it worked for different types :)

--- src/main/kotlin/quetion 4.kt
+++ src/main/kotlin/quetion 4.kt
@@ -1,5 +1,13 @@
-fun ArrayList<Any?>.print(array:ArrayList<Any?>){
-    array.forEach { item -> println(item.toString()) }
+fun ArrayList<Any?>.print(){
+    this.forEach { item -> println(item.toString()) }
 }
+class Person
 fun main(){
+    var testArray=ArrayList<Any?>()
+    testArray.add("apple")
+    testArray.add(5)
+    testArray.add(true)
+    var p=Person()
+    testArray.add(p)
+    testArray.print()
 }
