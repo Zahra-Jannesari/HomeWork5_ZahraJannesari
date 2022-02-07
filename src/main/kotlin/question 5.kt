cf909111152efdb86c15ef926223c3ba662c4e65
Zarisa <jannesari.zarisa@gmail.com>
2022-02-07 23:03:39 +0330

    implement method block

--- src/main/kotlin/question 5.kt
+++ src/main/kotlin/question 5.kt
@@ -1,3 +1,8 @@
 fun <T>List<T>.sliceAtGivenIndex(index:Int){
-
+    if(index in 0 until this.size){
+        var firstList=this.slice(0..index)
+        var secondList=this.slice(index+1 until this.size)
+    }else{
+        println("Index is greater than list size.")
+    }
 }
