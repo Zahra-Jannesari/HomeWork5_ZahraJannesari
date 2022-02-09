6dc203ad00dd9ea6e5d5bc673ae17ea6d0fa3934
Zarisa <jannesari.zarisa@gmail.com>
2022-02-09 22:26:52 +0330

    check if the number is not binary, print the number in binary form

--- src/main/kotlin/question 7.kt
+++ src/main/kotlin/question 7.kt
@@ -6,8 +6,24 @@ fun main(){
             var reader=Scanner(System.`in`)
             println("Enter a number (decimal or binary). ")
             var userNumber=reader.nextInt()
+            if (!isBinary(userNumber)){
+                println("$userNumber equals to ${Integer.toBinaryString(userNumber)}")
+                break
+            }else{
+
+            }
         } catch (e: Exception) {
             println("Enter a number!")
         }
     }
+}
+fun isBinary(number: Int): Boolean {
+    var binaryNumber = number
+    while (binaryNumber > 0) {
+        if (binaryNumber % 10 > 1) {
+            return false
+        }
+        binaryNumber /= 10
+    }
+    return true
 }
