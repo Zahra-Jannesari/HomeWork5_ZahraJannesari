054037ac59fd05ab7037ecc16583f33604201594
Zarisa <jannesari.zarisa@gmail.com>
2022-02-09 13:42:57 +0330

    remove val name of each interface

--- src/main/kotlin/question 1.kt
+++ src/main/kotlin/question 1.kt
@@ -1,20 +1,16 @@
 interface Vehicle{
-    val name:String
 }
 interface PublicTransportationVehicle:Vehicle {
     fun maxSpeed()
     fun maxCapacity()
-    override val name: String
 }
 interface ServiceTransportationVehicle:Vehicle {
     fun maxSpeed()
     fun maxCapacity()
-    override val name: String
 }
 interface PrivateTransportationVehicle:Vehicle {
     fun maxSpeed()
     fun maxCapacity()
-    override val name: String
 }
 fun main() {
 }
