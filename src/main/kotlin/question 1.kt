be98d51f3733f5d4404838febd6ef7d7187565fc
Zarisa <jannesari.zarisa@gmail.com>
2022-02-09 16:01:10 +0330

    make some changes in the way of implementation of interfaces and add classes of each interface

--- src/main/kotlin/question 1.kt
+++ src/main/kotlin/question 1.kt
@@ -1,16 +1,54 @@
-interface Vehicle{
-}
+interface Vehicle
 interface PublicTransportationVehicle:Vehicle {
-    fun maxSpeed()
-    fun maxCapacity()
+    fun maxSpeed():Int
+    fun maxCapacity():Int
 }
 interface ServiceTransportationVehicle:Vehicle {
-    fun maxSpeed()
-    fun maxCapacity()
+    fun maxSpeed():Int
+    fun maxCapacity():Int
 }
 interface PrivateTransportationVehicle:Vehicle {
-    fun maxSpeed()
-    fun maxCapacity()
+    fun maxSpeed():Int
+    fun maxCapacity():Int
+}
+
+class Taxi():PublicTransportationVehicle{
+    var maxSpeed=120
+    var maxCapacity=4
+    override fun maxSpeed(): Int { return maxSpeed }
+    override fun maxCapacity():Int {return maxCapacity}
+}
+class Bus():PublicTransportationVehicle{
+    var maxSpeed=60
+    var maxCapacity=50
+    override fun maxSpeed(): Int { return maxSpeed }
+    override fun maxCapacity():Int {return maxCapacity}
+}
+
+class Truck():ServiceTransportationVehicle{
+    var maxSpeed=50
+    var maxCapacity=2
+    override fun maxSpeed(): Int { return maxSpeed }
+    override fun maxCapacity():Int {return maxCapacity}
+}
+class Ambulance():ServiceTransportationVehicle{
+    var maxSpeed=200
+    var maxCapacity=5
+    override fun maxSpeed(): Int { return maxSpeed }
+    override fun maxCapacity():Int {return maxCapacity}
+}
+
+class Van():PublicTransportationVehicle{
+    var maxSpeed=100
+    var maxCapacity=10
+    override fun maxSpeed(): Int { return maxSpeed }
+    override fun maxCapacity():Int {return maxCapacity}
+}
+class SportCar():PublicTransportationVehicle{
+    var maxSpeed=120
+    var maxCapacity=4
+    override fun maxSpeed(): Int { return maxSpeed }
+    override fun maxCapacity():Int {return maxCapacity}
 }
 fun main() {
 }
--- src/main/kotlin/question 2.kt
+++ src/main/kotlin/question 2.kt
@@ -1,25 +1,33 @@
-abstract class Employee(var name: String,var age: Int, var hourRate: Double){
- abstract fun salary(hours: Double): Double
- override fun toString(): String {
-  return "name: $name age: $age hourRate: $hourRate"
- }
-}
-class Manager(name: String="",age: Int=20,hoursRate: Double=0.0):Employee(name,age,hoursRate){
- override fun salary(hours: Double): Double {
-  return hourRate*hours
- }
-}
-class  Clerk(name: String="",age: Int=20,hoursRate: Double=0.0):Employee(name,age,hoursRate){
- override fun salary(hours: Double): Double {
-  return hourRate*hours/2
- }
-}
-data class Customer(var name:String, var age:Int,var gender:Gender){
-
-}
-enum class Gender{
- Male,Female
-}
-fun main(){
+abstract class Employee(var name: String, var age: Int, var hourRate: Double) {
+    val ListOfPayments=ArrayList<Double>()
+    abstract fun salary(hours: Double): Double
+    override fun toString(): String {
+        return "name: $name age: $age hourRate: $hourRate"
+    }
+ fun getTotalPayment(){
 
  }
+}
+
+class Manager(name: String = "", age: Int = 20, hoursRate: Double = 0.0) : Employee(name, age, hoursRate) {
+    override fun salary(hours: Double): Double {
+        return hourRate * hours
+    }
+}
+
+class Clerk(name: String = "", age: Int = 20, hoursRate: Double = 0.0) : Employee(name, age, hoursRate) {
+    override fun salary(hours: Double): Double {
+        return hourRate * hours / 2
+    }
+}
+
+data class Customer(var name: String, var age: Int, var gender: Gender) {
+}
+
+enum class Gender {
+    Male, Female
+}
+
+fun main() {
+
+}
