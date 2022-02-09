14b8789b91be7b30be7877a54f0ba612bd50fbb1
Zarisa <jannesari.zarisa@gmail.com>
2022-02-09 13:27:33 +0330

    add answer for part 3

--- src/main/kotlin/question 6.kt
+++ src/main/kotlin/question 6.kt
@@ -3,7 +3,7 @@ fun main(){
     var n = 5
     println("Your input is: $n")
     /*
-    complexity of this piece of code is O(1):a constant amount of time is needed for code to run.
+    complexity of this piece of code is O(1):a constant amount of time is needed for code to run, not related to n.
     the code repeats just one time and prints a very simple sentence:"Your input is: 5"
     */
 
@@ -21,7 +21,11 @@ fun main(){
         for(j in 1..n)
             println("Your output is $i and $j")
         }
-
+    /*
+    complexity of this piece of code is O(n^2)=O(5^2)=O(25):two nested loops on n inputs
+    time depends on 'n' to the power of 2(number of loops). A constant amount of time multiply by 5 to the power of 2 is needed for code to run.
+    the code repeats n^2=25 time and each time prints a sentence.
+    */
 
     fun factorial(n:Int):Int{
         return if (n >= 1)
